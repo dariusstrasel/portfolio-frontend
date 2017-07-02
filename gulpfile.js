05908e505f7d38cc5fbcfe9bc85a395fccb5054f
Darius Strasel <strasel.darius@gmail.com>
2017-07-02 16:30:28 -0400

    Lint code.

--- gulpfile.js
+++ gulpfile.js
@@ -3,56 +3,56 @@
  */
 
 // Gulp.js configuration
-    var gulp = require('gulp');
-    var newer = require('gulp-newer');
-    var imagemin = require('gulp-imagemin');
-    var htmlclean = require('gulp-htmlclean');
-    var concat = require('gulp-concat');
-    var deporder = require('gulp-deporder');
-    var stripdebug = require('gulp-strip-debug');
-    var uglify = require('gulp-uglify');
-    var sass = require('gulp-sass');
-    var postcss = require('gulp-postcss');
-    var assets = require('postcss-assets');
-    var autoprefixer = require('autoprefixer');
-    var mqpacker = require('css-mqpacker');
-    var cssnano = require('cssnano');
+var gulp = require('gulp');
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
+var htmlclean = require('gulp-htmlclean');
+var concat = require('gulp-concat');
+var deporder = require('gulp-deporder');
+var stripdebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var assets = require('postcss-assets');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var cssnano = require('cssnano');
 
-    var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
 
-    // development mode?
-    var devBuild = (process.env.NODE_ENV !== 'production');
+// development mode?
+var devBuild = false; //(process.env.NODE_ENV !== 'production');
 
-    // folders
-    var folder = {
-        src: 'src/',
-        build: 'build/'
-    };
+// folders
+var folder = {
+    src: 'src/',
+    build: 'build/'
+};
 
 // image processing
-    gulp.task('images', function () {
-        var out = folder.build + 'images/';
-        return gulp.src(folder.src + 'images/**/*')
-            .pipe(newer(out))
-            .pipe(imagemin({ optimizationLevel: 5}))
-            .pipe(gulp.dest(out));
-    });
+gulp.task('images', function() {
+    var out = folder.build + 'images/';
+    return gulp.src(folder.src + 'images/**/*')
+        .pipe(newer(out))
+        .pipe(imagemin({ optimizationLevel: 10 }))
+        .pipe(gulp.dest(out));
+});
 
 // HMTL processing
-    gulp.task('html', ['images'], function () {
-        var
-            out = folder.build,
-            page = gulp.src(folder.src + 'html/**/*')
-                .pipe(newer(out));
-        if (!devBuild) {
-            page = page.pipe(htmlclean());
-        }
+gulp.task('html', ['images'], function() {
+    var
+        out = folder.build,
+        page = gulp.src(folder.src + 'html/**/*')
+        .pipe(newer(out));
+    if (!devBuild) {
+        page = page.pipe(htmlclean());
+    }
 
-        return page.pipe(gulp.dest(out));
-    });
+    return page.pipe(gulp.dest(out));
+});
 
 // JavaScript processing
-gulp.task('js', function () {
+gulp.task('js', function() {
 
     var jsbuild = gulp.src(folder.src + 'js/**/*')
         .pipe(deporder());
@@ -67,7 +67,7 @@ gulp.task('js', function () {
 });
 
 // CSS processing
-gulp.task('css', ['images'], function () {
+gulp.task('css', ['images'], function() {
 
     var postCssOpts = [
         assets({ loadPaths: ['images/'] }),
@@ -93,7 +93,7 @@ gulp.task('css', ['images'], function () {
 
 gulp.task('run', ['html', 'css', 'js']);
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
 
     // image changes
     gulp.watch(folder.src + 'images/**/*', ['images']);
@@ -110,9 +110,9 @@ gulp.task('watch', function () {
 });
 
 
-gulp.task('browser-sync-watch', ['run'], function () {
+gulp.task('browser-sync-watch', ['run'], function() {
 
-        browserSync.init({
+    browserSync.init({
         server: {
             baseDir: folder.build
         }
